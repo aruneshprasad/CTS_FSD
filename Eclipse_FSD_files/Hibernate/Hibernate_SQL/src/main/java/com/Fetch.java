73c52de0543933c71ee0619a3a7b28ef6f2b2385
Arunesh <arunesh.prasad002@gmail.com>
2018-12-21 19:34:18 +0530

    Books

---
+++ Eclipse_FSD_files/Hibernate/Hibernate_SQL/src/main/java/com/Fetch.java
@@ -0,0 +1,27 @@
+package com;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.utils.HibernateUtils;
+
+public class Fetch {
+
+	public static void main(String[] args) {
+
+		SessionFactory sf = HibernateUtils.getSessionFactory();
+		Session ses = sf.openSession();
+
+		Employee emp = (Employee) ses.get(Employee.class, 1);
+
+		System.out.println("Id: " + emp.getEmpId());
+		System.out.println("Name: " + emp.getEmpName());
+		System.out.println("Salary: " + emp.getSalary());
+		System.out.println("Date of Birth: " + emp.getDob());
+		System.out.println("Department Id: " + emp.getDept().getDeptId());
+		System.out.println("Department Name: " + emp.getDept().getDeptName());
+
+		HibernateUtils.shutdown();
+	}
+
+}
--- Eclipse_FSD_files/Hibernate/Hibernate_SQL/src/main/java/com/Save.java
+++ Eclipse_FSD_files/Hibernate/Hibernate_SQL/src/main/java/com/Save.java
@@ -11,24 +11,19 @@ import com.utils.HibernateUtils;
 public class Save {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		SessionFactory sf = HibernateUtils.getSessionFactory();
 		Session ses = sf.openSession();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		
-		//Employee emp1 = new Employee("Arunesh", 45000, sdf.parse("1997-11-07"));
-		Employee emp2 = new Employee("Raj", 44000, sdf.parse("1997-10-07"));
-		Employee emp3 = new Employee("Ajay", 46000, sdf.parse("1997-05-09"));
-		Employee emp4 = new Employee("Pankaj", 47000, sdf.parse("1997-12-10"));
-		Employee emp5 = new Employee("Tejveer", 48000, sdf.parse("1997-09-25"));
-		
-		ses.save(emp2);
-		ses.save(emp3);
-		ses.save(emp4);
-		ses.save(emp5);
-		
+
+		Employee emp = new Employee("Arunesh", 45000, sdf.parse("1997-11-07"));
+		Department dept = new Department(100, "Accounts");
+		emp.setDept(dept);
+
+		ses.save(emp);
+
 		ses.beginTransaction().commit();
-		
+
 		HibernateUtils.shutdown();
 
 	}
